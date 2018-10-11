ea8858f98e41c058ee4d97a1917d36a6d3fac375
Mary Ogbe <maryogbe09@yahoo.com>
2018-10-11 22:21:07 +0100

    working on webpack setup

---
+++ build-utils/webpack.common.js
@@ -0,0 +1,12 @@
+// const path = require("path");
+
+// const config = {
+//         entry: "./src/",
+//         mode: 'production',
+//         output:{
+//             filename: "bundle.js",
+//             path: path.join(__dirname,"dist") //we are joining our tells it what path to output
+//         }};
+
+//     module.exports= config;
+        
--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,37 @@
 const path = require("path");
 const ExamplePluggin = require("./Examplepluggin");
 // const webpack = require("webpack");
+const commonConfig = require("./build-utils/webpack.common");
 
-module.exports ={
-    entry: "./src/index.js",
+module.exports = {
+    entry: "./src/",
     mode: 'production',
-
     output:{
         filename: "bundle.js",
         path: path.join(__dirname,"dist") //we are joining our tells it what path to output
-        },
+    },
+// (env) => {
+//     console.log(env);
+
+
+//     return commonConfig();
     
+// };
     //here we are allowing the webpack to manage every asset even if they are not bundled together
         module:{
-            rules:[
-                {
-                    // all cases have 2objects 
-                    //test tells webpack when i come across a file before it is added to the dependancy graph match against this regular expression and perform a certain transform;
-                    test: /\.js$/,
-                    //a babel-loader is an example of transform loader
-                    use: "file-loader"
-                },
-                {
-                    //loaders functionally transforms thereselves from right to left
-                    test:/\.css$/,
-                    use:[
-                        {loader:"style-loader"},
+             rules:[
+                 {
+                     // all cases have 2objects 
+                   //test tells webpack when i come across a file before it is added to the dependancy graph match against this regular expression and perform a certain transform;
+                     test: /\.js$/,
+                     //a babel-loader is an example of transform loader
+                     use: "file-loader"
+                 },
+               {
+                     //loaders functionally transforms thereselves from right to left
+                     test:/\.css$/,
+                  use:[
+                       {loader:"style-loader"},
                         {loader:"css-loader"}
                     ]
                 },
@@ -42,8 +48,8 @@ module.exports ={
                     ]
                 },
             ],
-                // plugins: [ 
-                //     new Examplepluggin(),
-                // ],
-            }
-        };
+        //         // plugins: [ 
+        //         //     new Examplepluggin(),
+        //         // ],
+        //     }
+        }}
